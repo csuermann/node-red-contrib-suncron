c9cfe49ef3ca5000479e333dd0a573aa137788b3
cornelius <cornelius.suermann@gmail.com>
2019-09-08 15:25:40 +0200

    make it work

--- suncron.js
+++ suncron.js
@@ -7,7 +7,129 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config)
 
     const node = this
-    let crons = []
+
+    const eventTypes = [
+      'sunrise',
+      'sunriseEnd',
+      'goldenHourEnd',
+      'solarNoon',
+      'goldenHour',
+      'sunsetStart',
+      'sunset',
+      'dusk',
+      'nauticalDusk',
+      'night',
+      'nadir',
+      'nightEnd',
+      'nauticalDawn',
+      'dawn'
+    ]
+
+    let msgCrons = []
+    let dailyCron = []
+
+    const calcScheduleForToday = function () {
+      let today = new Date()
+      let sunTimes = SunCalc.getTimes(today, config.lat, config.lon)
+
+      return eventTypes.reduce((result, eventType) => {
+        const payload = config[`${eventType}Payload`]
+
+        if (payload !== '') {
+          const payloadType = config[`${eventType}PayloadType`]
+          const topic = config[`${eventType}Topic`]
+          const sunEventTime = dayjs(sunTimes[eventType])
+          const offsetSec = config[`${eventType}Offset`]
+          const offsetType = config[`${eventType}OffsetType`]
+          const offset = offsetSec * offsetType
+          let cronTime
+
+          if (offset > 0) {
+            cronTime = dayjs(sunEventTime).add(offsetSec, 'second')
+          } else {
+            cronTime = dayjs(sunEventTime).subtract(offsetSec, 'second')
+          }
+
+          result[eventType] = {
+            event: eventType,
+            sunEventTimeUTC: sunEventTime.toISOString(),
+            sunEventTimeLocal: sunEventTime.format('YYYY-MM-DDTHH:mm:ss'),
+            offset: offsetSec * offsetType,
+            cronTime,
+            cronTimeUTC: cronTime.toISOString(),
+            cronTimeLocal: cronTime.format('YYYY-MM-DDTHH:mm:ss'),
+            payload,
+            payloadType,
+            topic
+          }
+        }
+        return result
+      }, {})
+    }
+
+    const formatSchedule = function (schedule) {
+      let result = {}
+      for (let eventType in schedule) {
+        let event = schedule[eventType]
+        result[eventType] = {
+          event: eventType,
+          sunEventTime: event.sunEventTimeLocal,
+          cronTime: event.cronTimeLocal
+        }
+      }
+      return result
+    }
+
+    const installMsgCronjobs = function (schedule) {
+      stopMsgCrons()
+
+      let i = 0
+
+      for (let eventType in schedule) {
+        i++
+
+        let event = schedule[eventType]
+
+        // override cronTimes for debugging purpose
+        if (RED.settings.suncronMockTimes) {
+          event.cronTime = dayjs().add(i * 5, 'second')
+        }
+
+        let cron = new CronJob({
+          cronTime: event.cronTime.toDate(),
+          onTick: () => {
+            ejectMsg(event, schedule)
+            setNodeStatus(`${event.event} at ${event.cronTime.format('HH:mm')}`)
+          }
+        })
+
+        try {
+          cron.start()
+          msgCrons.push(cron)
+        } catch (err) {
+          debug(`${event.event}: ${err.message}`)
+        }
+      }
+
+      debug(`${i} msg crons installed`)
+    }
+
+    const installDailyCronjob = function () {
+      // run daily cron 5 seconds past midnight (except for debugging: 5 seconds past the full minute)
+      const cronTime = RED.settings.suncronMockTimes
+        ? '5 * * * * *'
+        : '5 0 0 * * *'
+
+      const cron = new CronJob({
+        cronTime,
+        onTick: () => {
+          installMsgCronjobs(calcScheduleForToday())
+          setNodeStatus(`updated for ${dayjs().format('ddd, MMM D')}`)
+        }
+      })
+      cron.start()
+      return cron
+    }
 
     const debug = function (debugMsg) {
       if (RED.settings.suncronDebug) {
@@ -15,68 +137,26 @@ module.exports = function (RED) {
       }
     }
 
-    const setNodeStatus = function (text) {
+    const setNodeStatus = function (text, color = 'grey') {
       node.status({
-        fill: 'grey',
+        fill: color,
         shape: 'dot',
         text: text
       })
     }
 
-    const scheduleWindowCrons = function () {
-      stopCrons()
-
-      const now = dayjs()
-      const begin = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowBegin)
-      const end = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowEnd)
-      const windowBeginCronCallback = function () {
-        const schedule = stripPastBlocks(createSchedule(config))
-        debug(schedule)
-        const currentBlock = schedule.shift()
-        executeBlock(currentBlock)
-        scheduleMsgCrons(schedule)
-      }
-
-      if (isNowWithinWindow()) {
-        windowBeginCronCallback()
-      } else {
-        setNodeStatus(`next cycle: ${begin.format('HH:mm')}`)
-      }
-
-      windowBeginCron = new CronJob({
-        cronTime: `0 ${begin.minute()} ${begin.hour()} * * *`, // sec min hour dom month dow
-        onTick: windowBeginCronCallback
-      })
-      windowBeginCron.start()
-
-      windowEndCron = new CronJob({
-        cronTime: `0 ${end.minute()} ${end.hour()} * * *`, // sec min hour dom month dow
-        // cronTime: fakeCronTime,
-        onTick: () => {
-          setNodeStatus('cycle completed')
-        }
-      })
-      windowEndCron.start()
-
-      debug(
-        `window crontabs set up for ${begin.format('HH:mm')} and ${end.format(
-          'HH:mm'
-        )}`
-      )
-    }
-
-    const stopCrons = function () {
-      if (crons.length > 0) {
-        crons.forEach(cron => {
+    const stopMsgCrons = function () {
+      if (msgCrons.length > 0) {
+        msgCrons.forEach(cron => {
           cron.stop()
         })
 
-        debug(`${crons.length} crons deleted`)
-        crons = []
+        debug(`${msgCrons.length} msg crons deleted`)
+        msgCrons = []
       }
     }
 
-    const ejectMsg = function (isOn) {
+    const ejectMsg = function ({ payload, payloadType, topic }, schedule) {
       const castPayload = (payload, payloadType) => {
         if (payloadType === 'num') {
           return Number(payload)
@@ -89,29 +169,26 @@ module.exports = function (RED) {
         }
       }
 
-      let payload = isOn ? config.onPayload : config.offPayload
-      let payloadType = isOn ? config.onPayloadType : config.offPayloadType
-
       node.send({
-        topic: isOn ? config.onTopic : config.offTopic,
-        payload: castPayload(payload, payloadType)
+        topic,
+        payload: castPayload(payload, payloadType),
+        schedule: formatSchedule(schedule)
       })
     }
 
     node.on('close', function () {
-      stopCrons()
+      stopMsgCrons()
+      dailyCron.stop()
     })
     ;(function () {
       // on startup:
-      setNodeStatus('initializing...')
-      let today = new Date()
-      let times = SunCalc.getTimes(today, 52.479867, 13.336637)
-      let sunset = new Date(times.sunset)
 
-      debug('SUNSET: ' + sunset)
-      debug('TODAY: ' + today)
-      debug(times)
-      // console.log(times)
+      const schedule = calcScheduleForToday()
+      debug(schedule)
+
+      installMsgCronjobs(schedule)
+
+      dailyCron = installDailyCronjob()
     })()
   }
 
