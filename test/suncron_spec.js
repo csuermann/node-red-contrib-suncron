60cdf1681d1a4fe789d78a0b1b4a33ebe718b122
cornelius <cornelius.suermann@gmail.com>
2019-09-08 15:36:38 +0200

    add test

--- test/suncron_spec.js
+++ test/suncron_spec.js
@@ -1,22 +1,22 @@
 var should = require('should')
 var helper = require('node-red-node-test-helper')
 var suncron = require('../suncron.js')
+var demoFlow = require('./demo.json')
 
 helper.init(require.resolve('node-red'))
 
-describe('suncron Node', function() {
-  beforeEach(function(done) {
+describe('suncron Node', function () {
+  beforeEach(function (done) {
     helper.startServer(done)
   })
 
-  afterEach(function(done) {
+  afterEach(function (done) {
     helper.unload()
     helper.stopServer(done)
   })
 
-  it('should be loaded', function(done) {
-    var flow = [{ id: 'n1', type: 'suncron', name: 'suncron' }]
-    helper.load(suncron, flow, function() {
+  it('should be loaded', function (done) {
+    helper.load(suncron, demoFlow, function () {
       var n1 = helper.getNode('n1')
       n1.should.have.property('name', 'suncron')
       done()
