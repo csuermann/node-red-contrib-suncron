bf4ede35fc2b03dc352855c754f1b261374905fc
Benedek Kozma <cyberbeni@gmail.com>
2022-11-27 17:48:44 +0100

    sun time of day WIP

--- src/SunTimeOfDay.html.ts
+++ src/SunTimeOfDay.html.ts
@@ -1,4 +1,30 @@
+type SunTimeOfDayDataPoint = import('./SunTimeOfDayDef').SunTimeOfDayDataPoint
 type SunTimeOfDayEditorConfig = import('./SunTimeOfDayDef').SunTimeOfDayEditorConfig
+
+function exportDataPoint(dataPoint: HTMLElement): SunTimeOfDayDataPoint {
+	return {
+		event: 'midnight',
+		offset: 1440
+	}
+}
+
+const events = [
+	'midnight',
+	'dawn',
+	'dusk',
+	'goldenHour',
+	'goldenHourEnd',
+	'nadir',
+	'nauticalDawn',
+	'nauticalDusk',
+	'night',
+	'nightEnd',
+	'solarNoon',
+	'sunrise',
+	'sunriseEnd',
+	'sunset',
+	'sunsetStart'
+]
 
 RED.nodes.registerType<SunTimeOfDayEditorConfig>('sun-time-of-day', {
 	category: 'function',
@@ -15,7 +41,7 @@ RED.nodes.registerType<SunTimeOfDayEditorConfig>('sun-time-of-day', {
 				event: 'midnight',
 				offset: 1440
 			}
-			]
+		]
 		},
 		outputs: { value: 1 }
 	},
@@ -26,6 +52,7 @@ RED.nodes.registerType<SunTimeOfDayEditorConfig>('sun-time-of-day', {
 		return ''
 	},
 	icon: 'switch.svg',
+	paletteLabel: 'sun time of day',
 	label: function () {
 		if (this.name) {
 			return this.name
@@ -36,24 +63,66 @@ RED.nodes.registerType<SunTimeOfDayEditorConfig>('sun-time-of-day', {
 	},
 	oneditprepare: function () {
 		const node = this
-		$('#node-input-rule-container').css('min-height','150px').css('min-width','450px').editableList({
-			addItem: function(container,i,opt) {
-				// TODO
-			},
-			removeItem: function(opt) {
-				// TODO
-			},
-			sortItems: function(rules) {
-				// TODO
-			},
-			sortable: true,
-			removable: true
-		})
+		$('#node-input-rule-container')
+			.css('min-height','150px')
+			.editableList({
+				addItem: function(container,i,opt) {
+					const dataPoint: SunTimeOfDayDataPoint = (opt as any).dataPoint || { event: 'midnight', offset: 0 }
+					container.css({
+						overflow: 'hidden',
+						whiteSpace: 'nowrap',
+						display: 'flex',
+						'align-items': 'center'
+					})
+					const inputRows = $('<div></div>',{style:'flex-grow:1'}).appendTo(container)
+
+					const row1 = $('<div></div>',{style:'display: flex;'}).appendTo(inputRows)
+					row1.append($('<label><i class="fa fa-clock-o"></i> Event</label>'))
+					const eventPicker = $('<select/>',{style:'width:120px; text-align: center;'}).appendTo(row1)
+					for (const event of events) {
+						eventPicker.append($('<option></option>', {selected:(dataPoint.event == event)})
+							.val(event)
+							.text(event))
+					}
+
+					const row2 = $('<div></div>',{style:'display: flex; padding-top: 5px;'}).appendTo(inputRows)
+					row2.append($('<label><i class="fa fa-sliders"></i> Offset[min]</label>'))
+					row2.append($('<input/>', {type:'number'}))
+					// TODO
+				},
+				removeItem: function(opt) {
+					// TODO
+				},
+				sortItems: function(rules) {
+					// TODO
+				},
+				sortable: true,
+				removable: true
+			})
+
+		for (const dataPoint of node.dataPoints) {
+			$('#node-input-rule-container').editableList('addItem',{dataPoint})
+		}
 	},
 	oneditsave: function() {
-		// TODO
+		const node = this
+		const dataPoints = $('#node-input-rule-container').editableList('items')
+		node.dataPoints = []
+		for (const dataPoint of dataPoints) {
+			node.dataPoints.push(exportDataPoint(dataPoint))
+		}
+		$('#node-input-outputs')
+			.val(node.dataPoints.length > 1 ? node.dataPoints.length - 1 : 0)
 	},
 	oneditresize: function(size) {
-		// TODO
+		const rows = $('#dialog-form>div:not(.node-input-rule-container-row)')
+		let height = size.height
+		for (const row of rows) {
+			height -= $(row).outerHeight(true) || 0
+		}
+		const editorRow = $('#dialog-form>div.node-input-rule-container-row')
+		height -= (parseInt(editorRow.css('marginTop'))+parseInt(editorRow.css('marginBottom')))
+		height += 16
+		$('#node-input-rule-container').editableList('height',height)
 	}
 })
