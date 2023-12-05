53f0ba9d1be9ba10334da702844e7138c8695428
Benedek Kozma <cyberbeni@gmail.com>
2023-12-05 17:55:24 +0100

    update naming

--- src/html-utils/GetDisplayName.html.ts
+++ src/html-utils/GetDisplayName.html.ts
@@ -2,14 +2,14 @@ type SuncronDataPoint = import('../SunTimeOfDayDef').SuncronDataPoint
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getDisplayName(dataPoint: SuncronDataPoint): string {
-	if (dataPoint.offset == 0) {
+	if (dataPoint.offset == 0 && dataPoint.event != 'midnight') {
 		return dataPoint.event
 	} else {
 		const sign = dataPoint.offset > 0 ? '+' : '-'
 		const offset = Math.abs(dataPoint.offset)
 		const hour = Math.floor(offset / 60)
 		const min = offset % 60
-		if (dataPoint.event == 'midnight' && dataPoint.offset >= 0) {
+		if (dataPoint.event == 'midnight') {
 			return `${hour % 24}:${min.toLocaleString(undefined, {minimumIntegerDigits: 2})}`
 		}
 		let name = dataPoint.event + ' ' + sign
